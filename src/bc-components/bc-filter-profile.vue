c7a8f6b48b528adb158b2c3f68620c8bd459c317
Wellington Rodrigues de Oliveira <wellington.oliveira@digitro.com.br>
2019-03-27 15:13:44 -0300

    Inicio da criação do bc-filter-profile

---
+++ src/bc-components/bc-filter-profile.vue
@@ -0,0 +1,53 @@
+<template>
+    <div class="bc-filter-profile field">
+        <select name="profiles" class="inp" @change="fireProfileSelected">
+            <option value="" disabled :selected="selectedProfileId === null">{{'select' | i18n}}</option>
+            <option v-for="(profile, idx) in profiles"
+                    :value="idx"
+                    :selected="idx === selectedProfileId">
+                {{profile.label}}
+            </option>
+        </select>
+
+    </div>
+</template>
+
+<script>
+    import metadata from './metadata.json';
+    import i18n from './utils/i18n.js';
+
+    export default {
+        name: 'bc-filter-profile',
+        mixins: [i18n.mixin],
+        props: {
+            profiles: Array
+        },
+        data() {
+            return {
+                metadata,
+                selectedProfileId: null,
+                enableDefaultOption: true
+            };
+        },
+        methods: {
+            fireProfileSelected(e) {
+                this.$emit('change', e);
+            },
+            fireProfileRenamed() {
+
+            },
+            fireProfileSaved() {
+
+            },
+            fireProfileSavedAs() {
+
+            },
+            fireProfileRemoved() {
+
+            },
+            fireProfileDefault() {
+
+            }
+        }
+    };
+</script>
